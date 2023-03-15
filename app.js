5ab4ee1c17eef6713e6b4ce78dfb4d59ecd221f4
Nikola Milanović <nikola.milanovic.dev@gmail.com>
2023-03-15 17:38:43 +0100

    Initial commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const https = require("https");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -60,6 +62,6 @@ app.post("/failure", function(req, res) {
 });
 
 // The "process" object is defined by the hosting service, such as HEROKU
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Server is running");
+app.listen(port, function() {
+    console.log(`Server is running on port ${port}`);
 });
